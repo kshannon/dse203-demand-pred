d566e1deee9dea93abdc5015be749546210c16bd
Ehab Abdelmaguid <eabdelma@eng.ucsd.edu>
2017-10-28 00:33:28 -0700

    Add files via upload

--- query-code/dataint.py
+++ query-code/dataint.py
@@ -4,6 +4,9 @@ from datetime import datetime, date, time
 from sqlalchemy import create_engine
 import json
 import requests
+import pandas as pd
+import numpy as np
+import wrap as w
 
 # To setup:
 # - might have used pip and not conda to install
@@ -29,32 +32,49 @@ app = Flask(__name__)
 @app.route('/orders')
 def orders():
     # NOTE: Username, password, database
-    engine = create_engine('postgresql://postgres:password@localhost:5433/SQLBook')
-    conn = engine.connect()
-    result = conn.execute('''with t as (
-        select * 
-        from orders
-        limit 5 )
-        select to_json(array_agg(t)) from t''')
-    
-    r = result.fetchone()
-    conn.close()
-    return json.dumps(r[0])
+    df = w.pgdbWrap('''select * from orders limit 5''')
+    return df.to_json(orient='records')
 
 @app.route('/reviews')
 def reviews():
-    statement = '''USE TinySocial;
-                SELECT VALUE r
-                FROM reviews r
-                LIMIT 5 ;'''
-    payload = {
-                'statement': statement
-            }
+    df = w.asterixDBWrap("TinySocial","SELECT VALUE r FROM reviews r LIMIT 5 ;")
+    results = df.to_json(orient='records')
+    return results
 
-    a_response = requests.post('http://localhost:19002/query/service', data = payload)
-    print a_response.status_code
-    q = a_response.json()
-    return json.dumps(q) ;
+@app.route('/sampleorderlines')
+def sampleorderlines():
+    
+    # These could be run in parallel.
+    # Or, could only look up in asterix the nodeid that are part of the sampling
+    
+    # 1) Get sampled orderlines
+    sql = '''
+    SELECT o.*, p.nodeid, p.asin 
+    FROM orderlines o TABLESAMPLE BERNOULLI(1) REPEATABLE(200)
+    inner join products p on o.productid = p.productid
+    '''
+
+    orderlines_df = w.pgdbWrap(sql)
+    # creating a temporary column to hold the integer value of nodeid for joining/merging
+    orderlines_df['nodeid'] = orderlines_df['nodeid'].apply(np.int64)
+
+    # 2) get classificatin data. this could be cached as we are using it alot
+    classification_df = w.asterixDBWrap("TinySocial",
+            '''SELECT r.nodeID, r.classification
+            FROM ClassificationInfo r;''')
+    o_df  = pd.merge(orderlines_df, 
+                     classification_df[['classification','nodeID']], 
+                     left_on='nodeid', right_on='nodeID')
+    #o_df.drop('_nodeid')
+
+    results = o_df.to_json(orient='records')
+    return results
+
+@app.route('/reviewer')
+def reviewerid():
+    d = {'q': '*:*', 'wt': 'json', 'fq': "reviewerID:\'AH2L9G3DQHHAJ\'"} 
+    d_res1=solrWrap('bookstore',d)
+    return d_res1
 
 if __name__ == '__main__':
     app.run(threaded=True)
---
+++ query-code/wrap.py
@@ -0,0 +1,38 @@
+import pysolr
+import pandas as pd
+import numpy as np
+import json
+import requests
+from datetime import datetime, date, time
+from sqlalchemy import create_engine
+
+def solrWrap(core,params):
+    query_string='http://localhost:8983/solr/'+core+'/select?'
+    for key in params:
+        query_string=query_string+key+'='+params[key]+'&'
+        #print (query_string)
+    solrcon = pysolr.Solr(query_string, timeout=10)
+    results = solrcon.search('*:*')
+    docs=pd.DataFrame(results.docs)
+    
+    return docs
+    
+def asterixDBWrap(dverse, query):
+    statement = 'USE '+dverse+';'+query
+    payload = {'statement': statement}
+    a_response = requests.post('http://localhost:19002/query/service', data = payload)
+    print "---------\n"
+    print (a_response.status_code)
+    print (a_response)
+    print "---------\n"
+
+    q = a_response.json()
+    
+    return pd.DataFrame(q['results'])
+    
+def pgdbWrap(qString):
+    engine = create_engine('postgresql://postgres:password@localhost:5433/SQLBook')
+    df = pd.read_sql(qString, engine)
+    engine.dispose()
+    
+    return df
